01f42c72050e129ef9decaee1bb8d7e576ee99a9
efournierrobert <elliott1447@gmail.com>
2025-11-26 17:27:23 -0500

    New function to set root user password

--- lib/user_management/user_management.py
+++ lib/user_management/user_management.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import arch_chroot as chroot
+
 
 class User:
     def __init__(self, username: str, password: str, homepath: Path, sudoer: bool):
@@ -10,3 +12,12 @@ class User:
 
     def to_string(self) -> str:
         return f"{self.username}:\nPassword: {self.password}\nHomepath: {self.homepath}\nIs Sudoer?: {self.sudoer}"
+
+
+def set_root_password(password: str) -> None:
+    command = f'echo "{password}" | passwd -s'
+
+    try:
+        chroot.run(command)
+    except chroot.ArchChrootExecutionError as e:
+        raise e
