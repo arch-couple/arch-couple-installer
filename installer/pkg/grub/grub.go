e8da178a348ab9631ffc62a9003ce771b3627822
efournierrobert <elliott1447@gmail.com>
2025-12-31 07:05:13 -0500

    Added doc

--- installer/pkg/grub/grub.go
+++ installer/pkg/grub/grub.go
@@ -9,6 +9,17 @@ import (
 const esp string = "/boot"
 const bootloaderId string = "GRUB"
 
+// Installs and sets up Grub on the newly installed system.
+//
+// Does:
+//   - grub-Install
+//   - uncommend os-prober line in /etc/default/grub
+//   - os-prober
+//   - grub-mkconfig
+//
+// Can return error types:
+//   - PipeError
+//   - ArchChrootError
 func InstallGrub() error {
 	if err := grubInstall(); err != nil {
 		return err
@@ -21,11 +32,18 @@ func InstallGrub() error {
 	return updateGrubConfig()
 }
 
+// Updates the current Grub config.
+//
+// Executes:
+//
+//	grub-mkconfig -o /boot/grub/grub.cfg
 func updateGrubConfig() error {
 	command := "grub-mkconfig -o /boot/grub/grub.cfg"
 	return arch_chroot.Run(command)
 }
 
+// Uncomments the os-prober line inside /etc/default/grub
+// and runs os-prober.
 func setUpOsProber() error {
 	sedCommand := "sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub"
 	osProberCommand := "os-prober"
@@ -33,6 +51,11 @@ func setUpOsProber() error {
 	return arch_chroot.Run(command)
 }
 
+// Runs the Grub installation on the new system.
+//
+// Executes:
+//
+//	grub-install...
 func grubInstall() error {
 	command := fmt.Sprintf("grub-install --target=x86_64-efi --efi-directory=%s --bootloader-id=%s", esp, bootloaderId)
 	return arch_chroot.Run(command)
